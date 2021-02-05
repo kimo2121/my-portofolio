12ebd9ac265c5689d4fa47216046668418790225
kimo2121 <kareem.gaad@gmail.com>
2021-02-05 07:54:12 +0200

    changed home page design

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 import "./portfolio.scss";
+import Data from "./projects-data";
+
 const Portfolio = () => (
   <div className="portfolio">
     <div className="poster">
@@ -8,133 +10,40 @@ const Portfolio = () => (
       <div className="explore">Feel free to explore</div>
       <div className="proj">Projects.</div>
     </div>
-    <div className="project-template">
-      <div className="container">
-        <div className="crwn"></div>
-      </div>
-      <div className="details">
-        <h4 className="h4">Project: crwn clothing </h4>
-        <h5 className="h5">
-          Tech Used: React.JS <br />
-          For state management: Redux & Redux-saga
-          <br />
-          For navigation: React-router-dom
-          <br />
-          For styling: Styled Components
-          <br />
-          For API Calls and Authintication: Firebase
-        </h5>
-        <div className="ref">
-          <div className="git-url">
-            URL:
-            <a className="a" href="https://crwn-live.herokuapp.com/">
-              https://crwn-live.herokuapp.com/
-            </a>
+    <div className="templates-container">
+      {Data.map((item) => (
+        <div className="project-template">
+          <div className="container">
+            <img className="image" src={item.image} alt="" />
           </div>
-          <div className="project-url">
-            Code:
-            <a className="a" href="https://github.com/kimo2121/Crwn-Clothing">
-              https://github.com/kimo2121/Crwn-Clothing
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="project-template">
-      <div className="container">
-        <div className="netflex"></div>
-      </div>
-      <div className="details">
-        <h4 className="h4">Project: Netflix clone </h4>
-        <h5 className="h5">
-          Tech Used: React.JS <br />
-          For state management: Hooks
-          <br />
-          For navigation: None
-          <br />
-          For styling: Css
-          <br />
-          For API Calls and Authintication: Axios
-        </h5>
-        <div className="ref">
-          <div className="project-url">
-            URL:
-            <a className="a" href="https://netflex-clone.herokuapp.com/">
-              https://netflex-clone.herokuapp.com/
-            </a>
-          </div>
-          <div className="git-url">
-            Code:
-            <a className="a" href="https://github.com/kimo2121/Netflix-clone">
-              https://github.com/kimo2121/Netflix-clone
-            </a>
+          <div className="details">
+            <h4 className="h4">Project: {item.name} </h4>
+            <h5 className="h5">
+              Tech Used: {item.tech}
+              <br />
+              For state management: {item.state_management}
+              <br />
+              For styling: {item.styling}
+              <br />
+              Authintication: {item.Authintication}
+            </h5>
+            <div className="ref">
+              <div className="git-url">
+                URL:
+                <a className="a" href={item.url}>
+                  {item.url}
+                </a>
+              </div>
+              <div className="project-url">
+                Code:
+                <a className="a" href={item.code}>
+                  {item.code}
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div className="project-template">
-      <div className="container">
-        <div className="lyrics"></div>
-      </div>
-      <div className="details">
-        <h4 className="h4">Project: Lyrics finder </h4>
-        <h5 className="h5">
-          Tech Used: React.JS <br />
-          For state management: Context API & Hooks
-          <br />
-          For navigation: React-router-dom
-          <br />
-          For styling: Css
-          <br />
-          For API Calls and Authintication: Axios
-        </h5>
-        <div className="ref">
-          <div className="project-url">
-            URL:
-            <a className="a" href="https://high-beats.herokuapp.com/">
-              https://high-beats.herokuapp.com/
-            </a>{" "}
-          </div>
-          <div className="git-url">
-            Code:
-            <a className="a" href="https://github.com/kimo2121/High-beats">
-              https://github.com/kimo2121/High-beats
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="project-template">
-      <div className="container">
-        <div className="calculator"></div>
-      </div>
-      <div className="details">
-        <h4 className="h4">Project: Calculator </h4>
-        <h5 className="h5">
-          Tech Used: React.JS <br />
-          For state management: Hooks
-          <br />
-          For navigation: None
-          <br />
-          For styling: Css
-          <br />
-          For API Calls and Authintication: None
-        </h5>
-        <div className="ref">
-          <div className="project-url">
-            URL:
-            <a className="a" href="https://ca-culator.herokuapp.com/">
-              https://ca-culator.herokuapp.com/
-            </a>{" "}
-          </div>
-          <div className="git-url">
-            Code:
-            <a className="a" href="https://github.com/kimo2121/my-calculator">
-              https://github.com/kimo2121/my-calculator
-            </a>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   </div>
 );
---
+++ src/components/projects-data.js
@@ -0,0 +1,59 @@
+import e_shopping from "../assets/e_shopping.png";
+import crwnclothing from "../assets/crwnclothing.png";
+import netflex from "../assets/netflex.png";
+import lyrics from "../assets/lyrics.png";
+import calc from "../assets/calc.png";
+
+const Data = [
+  {
+    image: e_shopping,
+    name: "e-shopping-time",
+    tech: "React",
+    state_management: "Redux",
+    styling: "scss",
+    Authintication: "GraphQL & Apollo",
+    url: "https://e-shopping-time.netlify.app/",
+    code: "https://github.com/kimo2121/e-commerce",
+  },
+  {
+    image: crwnclothing,
+    name: "crwn clothing",
+    tech: "React",
+    state_management: "Redux",
+    styling: "Styled components",
+    Authintication: "Firebase",
+    url: "https://crwn-live.herokuapp.com/",
+    code: "https://github.com/kimo2121/Crwn-Clothing",
+  },
+  {
+    image: netflex,
+    name: "Netflix clone",
+    tech: "React",
+    state_management: "local state(Hooks)",
+    styling: "Css",
+    Authintication: "none",
+    url: "https://netflex-clone.herokuapp.com/",
+    code: "https://github.com/kimo2121/Netflix-clone",
+  },
+  {
+    image: lyrics,
+    name: "Lyrics finder",
+    tech: "React",
+    state_management: "Context API & Hooks",
+    styling: "css",
+    Authintication: "none",
+    url: "https://high-beats.herokuapp.com/",
+    code: "https://github.com/kimo2121/High-beats",
+  },
+  {
+    image: calc,
+    name: "Calculator",
+    tech: "React",
+    state_management: "local state(Hooks)",
+    styling: "css",
+    Authintication: "none",
+    url: "https://ca-culator.herokuapp.com/",
+    code: "https://github.com/kimo2121/my-calculator",
+  },
+];
+export default Data;
